bea4289f141131d79e1bb66e92ed5cf69fcb81bd
Mávery Vargas <maveryvargas@gmail.com>
2023-02-13 20:59:51 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,28 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]//Instaciação de API
+[Microsoft.AspNetCore.Mvc.Route("[controller]")] // rota do controlador, usa-se "[controller]" para ser mais facil de fazer a manutenção
+
+public class FilmeController : ControllerBase
+{
+
+    private static List<Models.Filme> filmes = new List<Filme>();
+    [HttpPost] //post -> postar, adicionar informação
+    public void AdicionaFilme([FromBody]Filme filme) //from body e pra explicitar o corpo da requisição.
+    {
+        filmes.Add(filme);//adiciona filme
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+    [HttpGet] //Get -> leitura, pegar
+    public IEnumerable<Filme> RecuperarFilmes() //enumerador
+    {
+        return filmes;
+    }
+
+
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+
+    [Required (ErrorMessage = "O titulo é invalido") ] //Obriga preenchimento
+    [MaxLength(50, ErrorMessage = "O filme nao pode ter mais de 50 caracteres")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O genero é invalido")]
+    [MaxLength(15,ErrorMessage = "O genero nao pode ter mais de 15 caracteres")] //limita o tamanho dos caracteres
+    public string Genero { get; set; }
+    [Required]
+    [Range(50,700,ErrorMessage = "Minutagem deve ficar entre 50 a 700 minutos")] // limita um intervalo de valores
+    public float Duracao { get; set;}
+}
